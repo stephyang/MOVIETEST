ba8db799cd60d628a0ed9d3c75a251891db4c09c
stephyang <stephanie1635945@gmail.com>
2016-03-28 17:56:04 +0800

    movie

---
+++ MOVIETEST/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MOVIETEST
+//
+//  Created by stephanie yang on 2016/3/28.
+//  Copyright © 2016年 stephanie yang. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MOVIETEST/movie.swift
@@ -0,0 +1,9 @@
+//
+//  movie.swift
+//  MOVIETEST
+//
+//  Created by stephanie yang on 2016/3/28.
+//  Copyright © 2016年 stephanie yang. All rights reserved.
+//
+
+import Foundation
