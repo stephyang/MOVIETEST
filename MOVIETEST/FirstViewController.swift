3e4e1487b199b4fc0b256c826119f2b79d547f5c
stephyang <stephanie1635945@gmail.com>
2016-03-28 13:19:23 +0800

    Initial Commit

---
+++ MOVIETEST/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MOVIETEST
+//
+//  Created by stephanie yang on 2016/3/28.
+//  Copyright © 2016年 stephanie yang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MOVIETEST/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MOVIETEST
+//
+//  Created by stephanie yang on 2016/3/28.
+//  Copyright © 2016年 stephanie yang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
